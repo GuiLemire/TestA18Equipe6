fa4eed35a1c6a6ab4e9cc5aa6a4050be7bdb7e0f
Guillaume <onemademu@gmail.com>
2021-09-03 09:28:04 -0400

    adding the sout and cleaning

--- src/testingthetestfortestingpurpose/TestingTheTestForTestingPurpose.java
+++ src/testingthetestfortestingpurpose/TestingTheTestForTestingPurpose.java
@@ -16,9 +16,8 @@ public class TestingTheTestForTestingPurpose {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        YO TABARNAC
         
-        //Test test
+        System.out.println("BOnjour test test");
     }
     
 }
